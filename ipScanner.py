dbe3ee26bb5607939e63cca4d39bae6e854f5556
AlexProv <alex.provencher@usherbrooke.ca>
2015-03-28 00:07:35 -0400

    some bug fix and a little base for the main app

--- ipScanner.py
+++ ipScanner.py
@@ -5,18 +5,18 @@
 import os
 import time
 import sys
+import socket
+import subprocess
 from subprocess import Popen
 
 class IpScanner:
     ipRange = '192.168.0'
     listAlive = []
-    listNoReply = []
-    listError = []
 
-    def __init__(self, ipRange):
+    def __init__(self, ipRange='192.168.0'):
         self.ipRange = ipRange
-    def __init__ (self):
-        self.ipRange = '192.168.0' #Pour ne rien faire sinon sa prend un constructeur par default
+    #def __init__ (self):
+    #    self.ipRange = '192.168.0' #Pour ne rien faire sinon sa prend un constructeur par default
 
     def scan(self):
         self.listAlive = []
@@ -26,23 +26,49 @@ class IpScanner:
         for n in range(1,255): # start ping processes 
             ip = self.ipRange + ".%d" % n
             #remplie une list de subprocess a executer avec la commande ping
+            #le -c 3 permet de pas faire ping a l'infenie 
             processToDo.append((ip, Popen(['ping', '-c', '3', ip], stdout=devnull,stderr=devnull)))
         while processToDo:
-            for i, (ip, proc) in enumerate(processToDo):
+            for i, (ip, proc) in enumerate(processToDo[:]):
                 if proc.poll() is not None: # ping finished
                     #si le ping a repondu avec un code sans erreur on sais que l'IP est up 
                     #si il y a erreure on assume que l'IP n'est pas bon
                     processToDo.remove((ip, proc)) # this makes it O(n**2)
                     if proc.returncode == 0:
                         self.listAlive.append(ip)
-                    elif proc.returncode == 2:
-                        self.listNoReply.append(ip)
-                    else:
-                        self.listError.append(ip)
             time.sleep(.04) 
         devnull.close()
-
+        try:
+            self.listAlive.remove(socket.gethostbyname(socket.gethostname())) # so on ne se tir pas dans le pied
+        except:
+            None 
         return self.listAlive
 
-    def getAliveIps():
-        return self.listAlive
+    
+    def getOsForIp(self, ip):
+        answer = 'failed'
+        try:
+            devnull = open(os.devnull, 'wb')
+            process = Popen(['sudo','nmap','-v','-O','-sS',ip], stdout=subprocess.PIPE, stderr = devnull)
+            out,err = process.communicate()
+
+
+            #look in the text to get the OS 
+            lines = out.splitlines()
+            answer = 'Unknown'
+            for l in lines: 
+                if 'Running' in l:
+                    answer = l.split(' ', 1)
+
+            process.kill()
+        except: 
+            None
+        return answer
+
+    def getHostName(self,ip):
+        answer = 'Failed'
+        try:
+            return socket.gethostbyaddr(ip)[0]
+        except: 
+            return answer
+
--- secu.py
+++ secu.py
@@ -1,13 +1,45 @@
-import nmap #Dl link => https://pypi.python.org/pypi/python-nmap
-from subprocess import Popen, PIPE
+from ipScanner import IpScanner
+import sys
 
-scanner = nmap.PortScanner()
-scanner.scan('192.168.1.1/24','22') #search for the losers (users) on the network. 
+def main():
+    print 'by default the scanner will scan 192.168.0'
+    ip = ''
+    scanner = None
+    #try:
+        #ip = sys.argv[1]
+    #    scanner = IpScanner()
+    #    None
+    #except: 
+        #scanner = IpScanner()
+    scanner = IpScanner()                
+    targetlist = scanner.scan()
 
-losers = scanner.all_hosts()
-losersMac = []
-for l in losers:
-    losersMac.append(Popen(["arp", "-n", l], stdout=PIPE))
+    attacklist = []
+    #who to target? 
+    if '-i' in sys.argv:
+        while True:
+            print 'get the os for the specified IP addresse, write next to go to next step.'    
+            print targetlist            
+            ip = raw_input()
+            if ip == 'next':
+                break
+            else:
+                attackOS = scanner.getOsForIp(ip)
+                print attackOS
+
+            print 'do you want to add this to the attack list? (y/n)'
+            answer = raw_input()
+            if answer == 'y' or answer =='yes':
+                attacklist.append(ip)
+                targetlist.remove(ip)
 
 
-print repr(losers)
+    #peut faire la meme chose pour le hostname mais sa ne me retourne jamais rien de bon
+    else:
+        attacklist = targetlist
+        #iteraction avec usager pour les target
+    print attacklist
+
+    #launch atttackkk! 
+if __name__ == "__main__":
+    main()
